cff49e9fab60f57671d2e35ff54d266debfcff87
its-arun <arun.11601678@lpu.in>
2017-01-30 23:07:58 +0530

    Fixed v3.0

--- app/src/main/java/its/arun/miniums/MainActivity.java
+++ app/src/main/java/its/arun/miniums/MainActivity.java
@@ -23,13 +23,20 @@ public class MainActivity extends ActionBarActivity {
             WebSettings webSettings = myWebView.getSettings();
             webSettings.setJavaScriptEnabled(true);
             webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-            if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
-            webSettings.setAllowFileAccessFromFileURLs(true);
-            webSettings.setAllowUniversalAccessFromFileURLs(true);
-             }
-           // myWebView.loadUrl("https://its-arun.github.io/Mini-UMS/");
-            myWebView.loadUrl("file:///android_asset/index.html");
-            myWebView.setWebViewClient(new WebViewClient());
+            if (AppStatus.getInstance(this).isOnline(this)) {
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+                    webSettings.setAllowFileAccessFromFileURLs(true);
+                    webSettings.setAllowUniversalAccessFromFileURLs(true);
+                }
+                // myWebView.loadUrl("https://its-arun.github.io/Mini-UMS/");
+                myWebView.loadUrl("file:///android_asset/index.html");
+                myWebView.setWebViewClient(new WebViewClient());
+            }
+            else
+            {
+                Toast.makeText(getBaseContext(),
+                        "No Internet connection available", 4000).show();
+            }
             //Analytics Integration
             // Obtain the shared Tracker instance.
             GoogleAnalyticsApplication application = (GoogleAnalyticsApplication) getApplication();
